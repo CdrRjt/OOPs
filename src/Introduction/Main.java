68de32b2767858fe7aa2296f9160d2f96c197871
Rajat <cdrrjt@gmail.com>
2022-06-06 12:52:12 +0530

    Half way lecture one

--- src/Introduction/Main.java
+++ src/Introduction/Main.java
@@ -7,12 +7,29 @@ public class Main {
         int[] rol = new int[5];
 
         // teacher says to store name
-        String[] name = new String[5];
+        String[] names = new String[5];
 
         // Now teacher says to store data i.e. roll no. as well as name
-        int[] roll = new int[5];
-        String[] namse = new String[5];
-        float[] marks = new float[5];
+        // so we will create a class
+        //  Student Rajat; // here we are declaring the object . This is just a reference to an object of type student
+       // Rajat.name;    //  in the stack memory
+
+        // To create objects use new keyword which dynamically allocates memory at the runtime and returns reference
+        // variable to it.
+        Student Rajat = new Student();
+        Rajat.rollNo = 69;
+        Rajat.name = "Rajat Kumar";
+        Rajat.marks = 91.8f;
+        System.out.println(Rajat.rollNo);
+        System.out.println(Rajat.name);
+        System.out.println(Rajat.marks);
     }
 
+
+}
+
+class Student{
+    int rollNo;
+    String name;
+    float marks;
 }
