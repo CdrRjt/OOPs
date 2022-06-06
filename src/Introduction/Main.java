44a000e863a7aefb0157274a42796078d5a85d51
Rajat <cdrrjt@gmail.com>
2022-06-06 12:50:54 +0530

    First lecture half way

---
+++ src/Introduction/Main.java
@@ -0,0 +1,18 @@
+package Introduction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //teacher says store roll no.
+        int[] rol = new int[5];
+
+        // teacher says to store name
+        String[] name = new String[5];
+
+        // Now teacher says to store data i.e. roll no. as well as name
+        int[] roll = new int[5];
+        String[] namse = new String[5];
+        float[] marks = new float[5];
+    }
+
+}
